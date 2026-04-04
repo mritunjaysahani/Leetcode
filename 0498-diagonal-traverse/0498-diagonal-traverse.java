46528da0fd75735037ac410270afc2df91c1f545
Mritunjay Sahani <mrityunjayshn@gmail.com>
2026-04-04 06:40:32 +0530

    Time: 3 ms (73.84%), Space: 48.2 MB (56.57%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -1,32 +1,41 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
-       int m = mat.length;
-        int n = mat[0].length; 
-        int[] result = new int[m * n];
-        int i = 0, j = 0;
-        for (int k = 0; k < m * n; k++) {
-            result[k] = mat[i][j]; 
-            if ((i + j) % 2 == 0) {
-                if (j == n - 1) {
-                    i++;
-                } else if (i == 0) {
-                    j++;
-                } else {
-                    i--;
-                    j++;
-                }
-            } else {
-                if (i == m - 1) {
-                    j++;
-                } else if (j == 0) {
-                    i++;
-                } else {
-                    i++;
-                    j--;
-                }
+       int m=mat.length;
+       int n=mat[0].length;
+       int i=0,j=0;
+       int ans[]=new int [m*n];
+       for(int k=0;k<m*n;k++){
+        ans[k]=mat[i][j];
+        //now we have two conditions either going upword and either going downword
+        //both contains 3 cases
+        if((i+j)%2==0){
+            if(j==n-1){
+                i++;
             }
+            else if(i==0){
+                j++;
+            }
+            else{
+                i--;
+                j++;
+            }
+
         }
-        
-        return result;
+        else {
+            if(i==m-1){
+                j++;
+            }
+            else if(j==0){
+                i++;
+            }
+            else{
+                i++;
+                j--;
+            }
+
+        }
+       
+       } 
+       return ans;
     }
 }
