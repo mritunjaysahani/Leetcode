cb7ae2a60b57281176943f9d6a16df5b2cac86f9
Mritunjay Sahani <mrityunjayshn@gmail.com>
2026-04-05 07:19:44 +0530

    Time: 2 ms (99.92%), Space: 46.3 MB (91.07%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,20 +1,12 @@
 class Solution {
     public String convert(String s, int numRows) {
-int n = s.length();
-
-        if (numRows == 1 || n <= numRows) return s;
-
+      int n = s.length();
+      if (numRows == 1 || n <= numRows) return s;
         StringBuilder ans = new StringBuilder();
-
         for (int i = 0; i < numRows; i++) {
             int inc = 2 * (numRows - 1);
-
             for (int j = i; j < n; j += inc) {
-
-                // vertical element
                 ans.append(s.charAt(j));
-
-                // diagonal element (middle rows only)
                 int diag = j + inc - 2 * i;
                 if (i > 0 && i < numRows - 1 && diag < n) {
                     ans.append(s.charAt(diag));
