d4c263c4a015fc7ecb627ef3b8676d8041147874
Mritunjay Sahani <mrityunjayshn@gmail.com>
2026-04-06 13:25:31 +0530

    Time: 143 ms (46.73%), Space: 42.6 MB (97.49%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean solve(int cr,int cc,int ind,char board[][],String word ){
+        if(ind==word.length()) return true;
+        //base case
+        if(cr<0||cr>=board.length||cc<0||cc>=board[0].length||board[cr][cc]=='*'||board[cr][cc]!=word.charAt(ind)){
+            return false;
+        }
+        char temp=board[cr][cc];
+        board[cr][cc]='*';
+        boolean s1=solve(cr-1,cc,ind+1,board,word);
+        boolean s2=solve(cr+1,cc,ind+1,board,word);
+        boolean s3=solve(cr,cc-1,ind+1,board,word);
+        boolean s4=solve(cr,cc+1,ind+1,board,word);
+        board[cr][cc]=temp;
+        return s1 || s2 || s3 || s4;
+       
+    }
+    public boolean exist(char[][] board, String word) {
+        int ind=0;
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]==word.charAt(0)){
+                  boolean ans=  solve(i,j,0,board,word);
+                  if(ans) return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
