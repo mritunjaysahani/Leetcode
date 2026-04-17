5d5d3c82fa9f7af25c792ed52ffdf0c583f67938
Mritunjay Sahani <mrityunjayshn@gmail.com>
2026-04-17 14:12:24 +0530

    Time: 6 ms (75.03%), Space: 59.4 MB (81.51%) - LeetHub

---
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+     int n=nums.length,maxRamp=0;
+     int suffixArr[]=new int[n];
+     suffixArr[n-1]=nums[n-1];
+     for(int i=n-2;i>=0;i--){
+        suffixArr[i]=Math.max(suffixArr[i+1],nums[i]);
+     }
+     int i=0,j=0;
+     while(j<n){
+        while(i<j && nums[i]>suffixArr[j]) i++;
+        maxRamp=Math.max(maxRamp,j-i);
+        j++;
+     }
+     
+     return maxRamp;
+    }
+}
